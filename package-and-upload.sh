987e1765722e19b2e0f98a89df801b2bddcde65b
Vincent Zurczak <vincent.zurczak@gmail.com>
2016-07-29 17:18:45 +0200

    #106 Find a host for our snapshots and releases

---
+++ package-and-upload.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+#
+# This script should be invoked AFTER « gulp dist » was manually invoked.
+# It does not run the tests or verify the lint. But it...
+#
+# 1. ... invokes Gulp to build the distribution.
+# 2. ... makes a ZIP of the distribution.
+# 3. ... uploads it to Bintray.
+#
+
+# Constants
+readonly BINTRAY_URL="https://bintray.com/api/v1"
+readonly DIRECTORY="target/dist"
+
+readonly CONFIG="target/dev.config"
+readonly BACKUP="target/dev.config.backup"
+
+
+
+echo
+echo "Verifying the parameters and the build directory..."
+echo
+
+if [ -z $1  ]; then
+	echo "The version parameter is mandatory."
+	echo "Usage: package-and-upload.sh <version>"
+	echo "Use 'snapshot' for ANY snapshot version."
+	exit 1
+fi
+
+if [ -d "$CONFIG" ]; then
+	echo "$CONFIG cannot exist for releases and uploads!"
+	echo "Renaming it to $BACKUP."
+	mv $CONFIG $BACKUP || exit 1
+fi
+
+
+
+echo
+echo "Compiling the project..."
+echo
+
+ZIP="roboconf-web-administration-$1.zip"
+rm -f $ZIP
+gulp embed
+
+
+
+if [ -d "$BACKUP" ]; then
+	
+	echo
+	echo "Restoring the development configuration..."
+	echo
+	
+	mv $BACKUP $CONFIG || exit 1
+	echo "Done."
+fi
+
+
+
+echo
+echo "Zipping the content..."
+echo
+
+cd "$DIRECTORY" && zip -rq "../$ZIP" . && cd ..
+echo "Done."
+
+
+
+echo
+echo "Uploading the ZIP file to Bintray..."
+echo
+
+curl -X PUT -T $ZIP -u ${BINTRAY_USER}:${BINTRAY_API_KEY} \
+	-H "X-Bintray-Package:archives" \
+	-H "X-Bintray-Version:all" \
+	-H "X-Bintray-Publish:1" \
+	-H "X-Bintray-Override:1" \
+	-H "X-Bintray-Explode:0" \
+	-# -o "/tmp/curl-output.txt" \
+	${BINTRAY_URL}/content/roboconf/roboconf-web-administration/all/
+
+echo
+echo "$(</tmp/curl-output.txt)"
+echo
